99808412721be43a7e92b5fbd3973ee311b23d5e
jimmymuthoni <jimmymuthoni26@gmail.com>
2026-02-20 18:18:40 +0300

    feat: update on deployment pages

--- stadi-analytics/next.config.js
+++ stadi-analytics/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", 
   images: {
     unoptimized: true,
   },
+   basePath: "/stadi-research", 
+  assetPrefix: "/stadi-research/"
 };
 
 module.exports = nextConfig;
